06fb48c7b72a6635ce25bb5e52107bae130bb7ce
Robu David <david@devcentrehouse.eu>
2025-04-15 08:44:43 +0300

    commit

--- dist/dwd-website/server/angular-app-manifest.mjs
+++ dist/dwd-website/server/angular-app-manifest.mjs
@@ -13,16 +13,16 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-CN7426JB.js",
-      "chunk-E4WRMXXX.js"
+      "chunk-PZTE2FQD.js",
+      "chunk-IPAPKDGI.js"
     ],
     "route": "/home"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-KQ4XVQOP.js",
-      "chunk-E4WRMXXX.js"
+      "chunk-LO5JTCEK.js",
+      "chunk-IPAPKDGI.js"
     ],
     "route": "/about-us"
   },
@@ -55,13 +55,13 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 17900, hash: 'd00d21bdd2742ca6752355b880288e5b0f6522d2e433adda4b1d854740c9d29c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7058, hash: 'e8e4668e64edf1460cbba67492fdaa09cf9c5910a7625c77d04fbead2b6f7f37', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'about-us/index.html': {size: 97775, hash: '67b9ddb07c8b91466367500a1f882e7ca8999d2b8b0edb7ed2416ad9047e32f5', text: () => import('./assets-chunks/about-us_index_html.mjs').then(m => m.default)},
-    'terms/index.html': {size: 43895, hash: '0d054e975018877a9017fdc078230f272ee64469f9e468d9391357c6203e3465', text: () => import('./assets-chunks/terms_index_html.mjs').then(m => m.default)},
-    'cookies/index.html': {size: 46062, hash: '20de615d6563a4b73fff8833c63ee36849c6ef32684a1d4c4caccd6d0dfb64a8', text: () => import('./assets-chunks/cookies_index_html.mjs').then(m => m.default)},
-    'faqs/index.html': {size: 44187, hash: '2f5f4a122cb77a788cc12c350818d70af978da4e156ea5a0efeebb8df7ace564', text: () => import('./assets-chunks/faqs_index_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 111621, hash: 'c5c63ebada79c2f1917ec069dbd9d2f23549b5c47b95194d61cc362cafd56d3c', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'styles-GMAG773I.css': {size: 241019, hash: 'dMdVsZ2NyHY', text: () => import('./assets-chunks/styles-GMAG773I_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 34210, hash: 'd7c33f4537a237587230c861e6b3eb53c19d8a4812cab7493df4affcb167f848', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 23099, hash: 'f8015de606b1b56db7ba857113d2fbdbdf1fce38117763d5e577bd89bea81a20', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'cookies/index.html': {size: 61546, hash: '2b5983514d15e8e61e27fe83e328251deb709333501076561b143ad5639aef4b', text: () => import('./assets-chunks/cookies_index_html.mjs').then(m => m.default)},
+    'about-us/index.html': {size: 113259, hash: '52c2f7aef85bd1368c66cd72497d8a8de090167be564cb544204f132e2f94a96', text: () => import('./assets-chunks/about-us_index_html.mjs').then(m => m.default)},
+    'faqs/index.html': {size: 59671, hash: '1666ce85bc60a2b4ee0243aa43ed466133ca492b26dfcad65990098eda95430f', text: () => import('./assets-chunks/faqs_index_html.mjs').then(m => m.default)},
+    'terms/index.html': {size: 59379, hash: 'bc175b9b83b18d562a0fa79a235dc1b93376f83479d4fd2c282d1fcdd717595b', text: () => import('./assets-chunks/terms_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 127105, hash: '931fc07d5efa8ecfd9d3f8945ddd4a40597f714cd87bdca9d5cb151d510aaec3', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
+    'styles-NKXJIVUT.css': {size: 238308, hash: '5MHQ+D0qb+A', text: () => import('./assets-chunks/styles-NKXJIVUT_css.mjs').then(m => m.default)}
   },
 };
--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,20 +1,11 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
-import { appConfig } from './app.config';
-import { serverRoutes } from './app.routes.server';
-import { provideFirebaseApp } from '@angular/fire/app';
-import { initializeApp } from 'firebase/app';
-import { provideFirestore } from '@angular/fire/firestore';
-import { getFirestore } from 'firebase/firestore';
+import { mergeApplicationConfig, ApplicationConfig } from "@angular/core";
+import { provideServerRendering } from "@angular/platform-server";
+import { provideServerRouting } from "@angular/ssr";
+import { appConfig } from "./app.config";
+import { serverRoutes } from "./app.routes.server";
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideServerRouting(serverRoutes),
-    provideFirebaseApp(() => initializeApp({})),
-    provideFirestore(() => getFirestore()),
-  ],
+  providers: [provideServerRendering(), provideServerRouting(serverRoutes)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
